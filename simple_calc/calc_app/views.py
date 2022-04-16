80a7e1dcdb6d17b055e18d687b7fc93778644b4e
Abolaji Ezekiel <ezeboy122@gmail.com>
2022-04-16 10:36:51 +0100

    added static files

--- simple_calc/calc_app/views.py
+++ simple_calc/calc_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {'name': 'ezekiel'})
+
+
+def result(request):
+    num1 = int(request.GET['num1'])
+    num2 = int(request.GET['num2'])
+    result = num1 + num2
+
+    return render(request, 'result.html', {'result': result})
